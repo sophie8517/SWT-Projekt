14da53cdcb414cc05baca968695d9f4867d61575
mirek <mirek-kral@outlook.cz>
2021-11-06 13:47:04 +0100

    katalog_devel

--- src/main/java/WelcomeController.java
+++ src/main/java/WelcomeController.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kickstart.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
