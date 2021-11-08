7b48a70867061975af66b650b4b59a3c92697b05
Song Bai <45269853+InSuLaTi0N@users.noreply.github.com>
2021-11-08 15:31:38 +0100

    Update WelcomeControllerIntegrationTests.java

--- src/test/java/controller/WelcomeControllerIntegrationTests.java
+++ src/test/java/controller/WelcomeControllerIntegrationTests.java
@@ -26,7 +26,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(classes=WelcomeController)
 @AutoConfigureMockMvc
 class WelcomeControllerIntegrationTests {
 	@Autowired
