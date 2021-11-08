e61b7777034233b12b09bd4472e3b8d615218a2c
sophie8517 <sophie.schulze2@mailbox.tu-dresden.de>
2021-11-08 19:04:36 +0100

    set home page to home.html

--- src/main/java/kickstart/Lottery.java
+++ src/main/java/kickstart/Lottery.java
@@ -44,7 +44,7 @@ public class Lottery {
 		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
 			registry.addViewController(LOGIN_ROUTE).setViewName("login");
-			registry.addViewController("/").setViewName("index");
+			registry.addViewController("/").setViewName("home");
 		}
 	}
 
