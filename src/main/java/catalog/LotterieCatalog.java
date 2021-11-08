27f5c5191a5026c56e399de8b9c845623e100452
Mirek Král <74070898+mikr676c@users.noreply.github.com>
2021-11-08 18:25:21 +0100

    Delete LotterieCatalog.java

--- src/main/java/catalog/LotterieCatalog.java
+++
@@ -1,9 +0,0 @@
-package catalog_lottery;
-
-import org.salespointframework.catalog.Catalog;
-import org.springframework.data.domain.Sort;
-
-public interface LotterieCatalog extends Catalog<catalog_lottery.Item> {
-    static final Sort DEFAULT_SORT = Sort.by("productIdentifier").descending();
-
-}
