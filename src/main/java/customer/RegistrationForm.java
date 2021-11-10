fa0a3c8ce677c1fd6aa3169b375d416a662139bc
Song Bai <45269853+InSuLaTi0N@users.noreply.github.com>
2021-11-10 13:05:06 +0100

    Update RegistrationForm.java

--- src/main/java/customer/RegistrationForm.java
+++ src/main/java/customer/RegistrationForm.java
@@ -2,13 +2,18 @@ package customer;
 
 import org.javamoney.moneta.Money;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class RegistrationForm {
 	@NotEmpty(message = "{RegistrationForm.name.NotEmpty}") //
 	private final String name;
 
 	@NotEmpty(message = "{RegistrationForm.password.NotEmpty}") //
+	@Min(8)
+	@Pattern(regexp = "^[0-9].+", message = "{RegistrationForm.password.NoNumber}") //
+	@Pattern(regexp = "^[A-Z].+", message = "{RegistrationForm.password.NoUpperCase}") //
 	private final String password;
 
 	/*
