f6e57799f15d9f2c5c14c492b77c0a53ec6ac440
ninak1650 <92383579+ninak1650@users.noreply.github.com>
2021-12-01 19:56:40 +0800

    Create CatalogControllerWebIntegrationTest.java

---
+++ src/test/java/kickstart/catalog/CatalogControllerWebIntegrationTest.java
@@ -0,0 +1,46 @@
+package kickstart.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CatalogControllerWebIntegrationTest {
+
+	@Autowired MockMvc mvc;
+	@Autowired CatalogController controller;
+
+	@Test
+	void zahlennotterieMvcIntegrationTest() throws Exception {
+		mvc.perform(get("/zahlenlotterie")). //
+				andExpect(status().isOk()).//
+				andExpect(model().attribute("ticketcatalog", is(not(emptyIterable()))));
+	}
+
+	@Test
+	void footballMvcIntegrationTest() throws Exception {
+		mvc.perform(get("/football")). //
+				andExpect(status().isOk()).//
+				andExpect(model().attribute("footballcatalog", is(not(emptyIterable()))));
+	}
+
+	//TODO
+	/*
+	@Test
+	void footballadminMvcIntegrationTest() throws Exception {
+		mvc.perform(get("/footballadmin")). //
+				andExpect(status().isOk()).//
+				andExpect(model().attribute("footballcatalog", is(not(emptyIterable()))));
+	}
+
+	 */
+}
