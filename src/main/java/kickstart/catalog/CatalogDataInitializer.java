47efbfe34f0304dcde449114485752925537ebf3
sophie8517 <sophie.schulze2@mailbox.tu-dresden.de>
2021-12-15 17:57:24 +0100

    update catalog entries

--- src/main/java/kickstart/catalog/CatalogDataInitializer.java
+++ src/main/java/kickstart/catalog/CatalogDataInitializer.java
@@ -47,82 +47,80 @@ class CatalogDataInitializer implements DataInitializer {
 		lotteryCatalog.save(new Football("name2",LocalDateTime.of(LocalDate.of(2021,11,26),LocalTime.of(17,30)),
 				Money.of(10, EURO),ItemType.FOOTBALL,new Team("FC Augsburg"), new Team("FC Bayern München") ,
 				"1.Bundesliga","augsburg", "fcb"));
-		lotteryCatalog.save(new Football("name3",LocalDateTime.of(LocalDate.of(2021,11,24), LocalTime.of(17,32)),
-				Money.of(10, EURO),ItemType.FOOTBALL,new Team("TSG Hoffenheim"), new Team("RB Leipzig"),
-				"1.Bundesliga","hoffenheim", "rb"));
+		lotteryCatalog.save(new Football("name3",LocalDateTime.of(LocalDate.of(2021,12,17), LocalTime.of(20,30)),
+				Money.of(10, EURO),ItemType.FOOTBALL,new Team("FC Bayern München"), new Team("VfL Wolfsburg"),
+				"1.Bundesliga","fcb", "wolfsburg"));
 
 
-		lotteryCatalog.save(new Football("name4",LocalDateTime.of(LocalDate.of(2021,12,3), LocalTime.of(20,30)),
-				Money.of(12, EURO),ItemType.FOOTBALL,new Team("1.FC Union Berlin"), new Team("RB Leipzig"),
-				"1.Bundesliga","union", "rb"));
+		lotteryCatalog.save(new Football("name4",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(15,30)),
+				Money.of(12, EURO),ItemType.FOOTBALL,new Team("VfL Bochum"), new Team("1.FC Union Berlin"),
+				"1.Bundesliga","bochum", "union"));
 
-		lotteryCatalog.save(new Football("name5",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(15,30)),
-				Money.of(10, EURO),ItemType.FOOTBALL,new Team("TSG Hoffenheim"), new Team("Eintracht Frankfurt"),
-				"1.Bundesliga","hoffenheim", "eintracht"));
+		lotteryCatalog.save(new Football("name5",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(15,30)),
+				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Eintracht Frankfurt"), new Team("1. FSV Mainz 05"),
+				"1.Bundesliga","eintracht", "mainz"));
 
-		lotteryCatalog.save(new Football("name6",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(15,30)),
-				Money.of(13, EURO),ItemType.FOOTBALL,new Team("FC Augsburg"), new Team("VfL Bochum"),
-				"1.Bundesliga","augsburg", "bochum"));
+		lotteryCatalog.save(new Football("name6",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(15,30)),
+				Money.of(13, EURO),ItemType.FOOTBALL,new Team("TSG Hoffenheim"), new Team("Borussia Mönchengladbach"),
+				"1.Bundesliga","hoffenheim", "gladbach"));
 
-		lotteryCatalog.save(new Football("name7",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(15,30)),
-				Money.of(8, EURO),ItemType.FOOTBALL,new Team("Bayer Leverkusen 04"), new Team("SpVgg Greuther Fürth"),
-				"1.Bundesliga","leverkusen", "fuerth"));
+		lotteryCatalog.save(new Football("name7",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(15,30)),
+				Money.of(8, EURO),ItemType.FOOTBALL,new Team("SpVgg Greuther Fürth"), new Team("FC Augsburg"),
+				"1.Bundesliga","fuerth", "augsburg"));
 
-		lotteryCatalog.save(new Football("name8",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(15,30)),
-				Money.of(9, EURO),ItemType.FOOTBALL,new Team("Arminia Bielefeld"), new Team("1.FC Köln"),
-				"1.Bundesliga","bielefeld", "koeln"));
+		lotteryCatalog.save(new Football("name8",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(15,30)),
+				Money.of(9, EURO),ItemType.FOOTBALL,new Team("RB Leipzig"), new Team("Arminia Bielefeld"),
+				"1.Bundesliga","rb", "bielefeld"));
 
-		lotteryCatalog.save(new Football("name9",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(15,30)),
-				Money.of(10, EURO),ItemType.FOOTBALL,new Team("1.FSV Mainz 05"), new Team("VfL Wolfsburg"),
-				"1.Bundesliga","mainz", "wolfsburg"));
+		lotteryCatalog.save(new Football("name9",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(18,30)),
+				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Hertha BSC"), new Team("Borussia Dortmund"),
+				"1.Bundesliga","hertha", "bvb"));
 
-		lotteryCatalog.save(new Football("name10",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(18,30)),
-				Money.of(20, EURO),ItemType.FOOTBALL,new Team("Borussia Dortmund"), new Team("FC Bayern München"),
-				"1.Bundesliga","bvb", "fcb"));
+		lotteryCatalog.save(new Football("name10",LocalDateTime.of(LocalDate.of(2021,12,19), LocalTime.of(15,30)),
+				Money.of(20, EURO),ItemType.FOOTBALL,new Team("SC Freiburg"), new Team("Bayer 04 Leverkusen"),
+				"1.Bundesliga","freiburg", "leverkusen"));
 
-		lotteryCatalog.save(new Football("name11",LocalDateTime.of(LocalDate.of(2021,12,5), LocalTime.of(15,30)),
-				Money.of(14, EURO),ItemType.FOOTBALL,new Team("VfB Stuttgart"), new Team("Hertha BSC"),
-				"1.Bundesliga","stuttgart", "hertha"));
+		lotteryCatalog.save(new Football("name11",LocalDateTime.of(LocalDate.of(2021,12,19), LocalTime.of(17,30)),
+				Money.of(14, EURO),ItemType.FOOTBALL,new Team("1. FC Köln"), new Team("VfB Stuttgart"),
+				"1.Bundesliga","koeln", "stuttgart"));
 
-		lotteryCatalog.save(new Football("name12",LocalDateTime.of(LocalDate.of(2021,12,5), LocalTime.of(17,30)),
-				Money.of(8, EURO),ItemType.FOOTBALL,new Team("Borussia Mönchengladbach"), new Team("SC Freiburg"),
-				"1.Bundesliga","gladbach", "freiburg"));
+		//----2.Liga-------------
 
-		lotteryCatalog.save(new Football("name13",LocalDateTime.of(LocalDate.of(2021,12,3), LocalTime.of(18,30)),
-				Money.of(9, EURO),ItemType.FOOTBALL,new Team("SV Darmstadt 98"), new Team("Fortuna Düssedorf"),
-				"2.Bundesliga","darmstadt", "fortuna"));
+		lotteryCatalog.save(new Football("name13",LocalDateTime.of(LocalDate.of(2021,12,17), LocalTime.of(18,30)),
+				Money.of(9, EURO),ItemType.FOOTBALL,new Team("Holstein Kiel"), new Team("St. Pauli"),
+				"2.Bundesliga","holstein", "stpauli"));
 
-		lotteryCatalog.save(new Football("name14",LocalDateTime.of(LocalDate.of(2021,12,3), LocalTime.of(18,30)),
-				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Werder Bremen"), new Team("FC Erzgebirge Aue"),
-				"2.Bundesliga","bremen", "aue"));
+		lotteryCatalog.save(new Football("name14",LocalDateTime.of(LocalDate.of(2021,12,17), LocalTime.of(18,30)),
+				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Fortuna Düsseldorf"), new Team("SV Sandhausen"),
+				"2.Bundesliga","fortuna", "sandhausen"));
 
-		lotteryCatalog.save(new Football("name15",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(13,30)),
-				Money.of(8, EURO),ItemType.FOOTBALL,new Team("SV Sandhausen"), new Team("SC Paderborn"),
-				"2.Bundesliga","sandhausen", "paderborn"));
+		lotteryCatalog.save(new Football("name15",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(13,30)),
+				Money.of(8, EURO),ItemType.FOOTBALL,new Team("FC Erzgebirge Aue"), new Team("1. FC Nürnberg"),
+				"2.Bundesliga","aue", "nuernberg"));
 
-		lotteryCatalog.save(new Football("name16",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(13,30)),
-				Money.of(12, EURO),ItemType.FOOTBALL,new Team("Hansa Rostock"), new Team("FC Ingolstadt 04"),
-				"2.Bundesliga","hansa", "ingolstadt"));
+		lotteryCatalog.save(new Football("name16",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(13,30)),
+				Money.of(12, EURO),ItemType.FOOTBALL,new Team("FC Ingolstadt 04"), new Team("Dynamo Dresden"),
+				"2.Bundesliga","ingolstadt", "dynamo"));
 
-		lotteryCatalog.save(new Football("name17",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(13,30)),
-				Money.of(8, EURO),ItemType.FOOTBALL,new Team("1.FC Nürnberg"), new Team("Holstein Kiel"),
-				"2.Bundesliga","nuernberg", "holstein"));
+		lotteryCatalog.save(new Football("name17",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(13,30)),
+				Money.of(8, EURO),ItemType.FOOTBALL,new Team("SC Paderborn"), new Team("1.FC Heidenheim"),
+				"2.Bundesliga","paderborn", "heidenheim"));
 
-		lotteryCatalog.save(new Football("name18",LocalDateTime.of(LocalDate.of(2021,12,4), LocalTime.of(20,30)),
-				Money.of(14, EURO),ItemType.FOOTBALL,new Team("St. Pauli"), new Team("FC Schalke 04"),
-				"2.Bundesliga","stpauli", "schalke"));
+		lotteryCatalog.save(new Football("name18",LocalDateTime.of(LocalDate.of(2021,12,18), LocalTime.of(20,30)),
+				Money.of(14, EURO),ItemType.FOOTBALL,new Team("Hamburger SV"), new Team("FC Schalke 04"),
+				"2.Bundesliga","hsv", "schalke"));
 
-		lotteryCatalog.save(new Football("name19",LocalDateTime.of(LocalDate.of(2021,12,5), LocalTime.of(13,30)),
-				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Hannover 96"), new Team("Hamburger SV"),
-				"2.Bundesliga","hannover", "hsv"));
+		lotteryCatalog.save(new Football("name19",LocalDateTime.of(LocalDate.of(2021,12,19), LocalTime.of(13,30)),
+				Money.of(10, EURO),ItemType.FOOTBALL,new Team("Karlsruher SC"), new Team("Hansa Rostock"),
+				"2.Bundesliga","ksc", "hansa"));
 
-		lotteryCatalog.save(new Football("name20",LocalDateTime.of(LocalDate.of(2022,12,5), LocalTime.of(13,30)),
-				Money.of(13, EURO),ItemType.FOOTBALL,new Team("Dynamo Dresden"), new Team("Karlsruher SC"),
-				"2.Bundesliga","dynamo", "ksc"));
+		lotteryCatalog.save(new Football("name20",LocalDateTime.of(LocalDate.of(2021,12,19), LocalTime.of(13,30)),
+				Money.of(13, EURO),ItemType.FOOTBALL,new Team("SSV Jahn Regensburg"), new Team("SV Darmstadt 98"),
+				"2.Bundesliga","regensburg", "darmstadt"));
 
-		lotteryCatalog.save(new Football("name21",LocalDateTime.of(LocalDate.of(2022,12,5), LocalTime.of(13,30)),
-				Money.of(8, EURO),ItemType.FOOTBALL,new Team("1.FC Heidenheim"), new Team("SSV Jahn Regensburg"),
-				"2.Bundesliga","heidenheim", "regensburg"));
+		lotteryCatalog.save(new Football("name21",LocalDateTime.of(LocalDate.of(2021,12,19), LocalTime.of(13,30)),
+				Money.of(8, EURO),ItemType.FOOTBALL,new Team("Hannover 96"), new Team("Werder Bremen"),
+				"2.Bundesliga","hannover", "bremen"));
 
 
 	}
